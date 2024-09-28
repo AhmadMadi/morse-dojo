5b5e54327310c2c637286c11a058f64cc57a6e99
AhmadMadi <madi96.at.work@gmail.com>
2024-09-28 16:51:05 +0300

    Escape single qoute

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,7 +2,9 @@ export default function NotFound() {
     return (
       <div className="h-screen flex flex-col items-center justify-center">
         <h1 className="text-4xl font-bold">Page Not Found</h1>
-        <p className="text-gray-600 mt-4">Sorry, the page you're looking for doesn't exist.</p>
+        <p className="text-gray-600 mt-4">
+          Sorry, the page you&apos;re looking for doesn&apos;t exist.
+        </p>
       </div>
     );
   }
