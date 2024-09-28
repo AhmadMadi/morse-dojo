a0a2a5977c77fb31e63c8284e783ed854b23e387
AhmadMadi <madi96.at.work@gmail.com>
2024-09-28 14:34:50 +0300

    Create home page + edit home layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,18 @@
-import Image from "next/image";
-import ThemeToggler from "./ui/global/themeToggler";
+import Link from 'next/link';
 
-export default function Home() {
+export default function HomePage() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <button className="btn btn-primary">Primary</button>
-      <ThemeToggler />
-    </div>
+    <main className="flex flex-col items-center justify-center h-screen text-center p-4">
+      <h1 className="text-4xl font-bold text-foreground">Welcome to Morsedojo</h1>
+      <p className="text-lg text-gray-400 max-w-lg mt-4">
+        Master Morse code with interactive lessons and practice sessions. 
+        Get started now and track your progress as you become a Morse code expert.
+      </p>
+      <Link href="/practice">
+        <button className="btn btn-primary mt-6">
+          Get Started
+        </button>
+      </Link>
+    </main>
   );
 }
