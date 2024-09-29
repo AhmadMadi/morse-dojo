13d993b6a7d9cc14ff7b192a918c2f34c1bdddf3
AhmadMadi <madi96.at.work@gmail.com>
2024-09-29 20:02:24 +0300

    Minor edit to topnav bar

--- src/app/ui/global/topNav.tsx
+++ src/app/ui/global/topNav.tsx
@@ -1,32 +1,32 @@
+import { Icons } from "@/app/icons";
+
 export default function TopNav() {
-    return (
-<div className="navbar bg-base-100">
-  <div className="flex-1">
-    <a className="btn btn-ghost text-xl">Morse Dojo</a>
-  </div>
-  <div className="flex-none gap-2">
-    {/* <div className="dropdown dropdown-end">
-      <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-        <div className="w-10 rounded-full">
-          <img
-            alt="Tailwind CSS Navbar component"
-            src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+  return (
+    <div className="navbar bg-base-100 shadow-sm drop-shadow-lg md:rounded-3xl">
+      <div className="flex-1">
+        <a className="btn btn-ghost text-xl">test</a>
+      </div>
+      <div className="flex-none gap-2">
+        <div className="dropdown dropdown-end">
+          <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+            <div className="w-8">
+            <Icons.defaultUserIcon />
+            </div>
+          </div>
+          {/* <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+            <li>
+              <a className="justify-between">
+                Profile
+                <span className="badge">New</span>
+              </a>
+            </li>
+            <li><a>Settings</a></li>
+            <li><a>Logout</a></li>
+          </ul> */}
         </div>
       </div>
-      <ul
-        tabIndex={0}
-        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-        <li>
-          <a className="justify-between">
-            Profile
-            <span className="badge">New</span>
-          </a>
-        </li>
-        <li><a>Settings</a></li>
-        <li><a>Logout</a></li>
-      </ul>
-    </div> */}
-  </div>
-</div>
-    );
+    </div>
+  );
 }
