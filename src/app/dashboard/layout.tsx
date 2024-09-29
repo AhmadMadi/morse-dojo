d3b71c2ac4f6c0806c6d165fee0f39906671b6ea
AhmadMadi <madi96.at.work@gmail.com>
2024-09-29 20:03:02 +0300

    Dashboard layout changes + add morse code alphabet map

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,14 +1,16 @@
 // src/app/dashboard/layout.tsx
-import { ReactNode } from 'react';
+import React, { ReactNode } from 'react';
 import TopNav from '@/app/ui/global/topNav';
 
 export default function DashboardLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="">
-      <TopNav />
-      <main className="flex-1">
-        {children}
-      </main>
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="w-full max-w-5xl md:w-11/12 md:p-6"> {/* Adjusts for mobile and larger screens */}
+        <TopNav />
+        <main className="p-4 md:p-6">
+          {children}
+        </main>
+      </div>
     </div>
   );
 }
