89751aac2baffcfee4c119e39972586beb0ed6c9
AhmadMadi <madi96.at.work@gmail.com>
2024-09-29 20:01:40 +0300

    Home page changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,20 @@
+'use client';
+
 import Link from 'next/link';
+import Logo from './ui/global/Logo';
+import { useMemo } from 'react';
 
 export default function HomePage() {
+  const theme = useMemo(() => {
+    return localStorage.getItem('theme') || 'winter';
+  }, [localStorage.getItem('theme')]);
+
   return (
     <main className="flex flex-col items-center justify-center h-screen text-center p-4">
-      <h1 className="text-4xl font-bold text-foreground">Welcome to Morsedojo</h1>
+      <h1 className="text-4xl font-bold text-foreground">
+        <Logo theme={theme}/>
+        MorseDojo
+      </h1>
       <p className="text-lg text-gray-400 max-w-lg mt-4">
         Master Morse code with interactive lessons and practice sessions. 
         Get started now and track your progress as you become a Morse code expert.
